5247767a820bf2f3b0f43eb29763a53249fe2aee
Igor Bonato <igorbonato99@hotmail.com>
2021-06-02 22:27:30 -0300

    things

--- foot2.py
+++ foot2.py
@@ -3,11 +3,25 @@ from bs4 import BeautifulSoup
 import json
 from pandas import json_normalize
 
-# headers for be a nav
+# headers for be a navv
 headers = {'User-Agent': 'Mozilla/5.0'}
 
 players = ['messi', 'cristiano', 'neymar',
-           'kane', 'cavani', 'dybala', 'haaland', 'mbappe']
+           'kane', 'cavani', 'dybala', 'haaland',
+           'mbappe', 'ibrahimovic', 'luis suarez',
+           'hazard', 'ozil', 'de bruyne', 'lukaku',
+           'rashford', 'quagliarella', 'ribery', 'robben',
+           'gerard moreno', 'dzeko', 'van persie', 'mane',
+           'immobile', 'benzema', 'giroud', 'salah', 'aspas',
+           'muller', 'morata', 'vardy', 'bruno fernandes',
+           'heung-min', 'lacazette', 'aubameyang', 'depay',
+           'ben yedder', 'yilmaz', 'di maria', 'tadic', 'bale',
+           'kramaric', 'sancho', 'higuain', 'payet', 'luis muriel',
+           'lautaro', 'insigne', 'belotti', 'zapata', 'mertens',
+           'rooney', 'coutinho', 'alexis sanchez', 'ilicic',
+           'milik', 'icardi', 'werner', 'raul jimenez', 'danny ings',
+           'sterling', 'aguero', 'tevez', 'firmino', 'david silva',
+           'huntelaar']
 
 url = "https://www.transfermarkt.com/schnellsuche/ergebnis/schnellsuche?query="
 urls = []
@@ -16,6 +30,7 @@ base = "http://www.transfermarkt.com/"
 
 
 def get_stats(urls):
+    """ entra na url de cada jogador e coleta todos stats """
     players = {"Players": []}
 
     for url in urls:
@@ -56,19 +71,24 @@ def get_stats(urls):
     return players
 
 
-def read_json(great_dict, filename):
+def read_json(great_dict, filename, excel=False):
+    """ função pega dict com stats dos jogadores e cria um arquivo json ou excel facultativo """
     file_json = filename+".json"
     with open(file_json, 'w') as outfile:
         json.dump(great_dict, outfile, indent=4)
-    conv_xml(file_json, filename+"z.xlsx")
+    if excel:
+        conv_xml(file_json, filename+"x.xlsx")
 
 
 def conv_xml(file_json, filename):
+    """ cria um xml a partir do json """
     with open(file_json, 'r') as infile:
         d = json.load(infile)
         normed = json_normalize(d, record_path=["Players"])
         normed.to_excel(filename, index=False, sheet_name="Stats Players")
 
+
+# looping de busca dos jogadores no site
 
 for player in players:
     k = 0
@@ -82,12 +102,18 @@ for player in players:
 
     urls.append(i)
     player_page = BeautifulSoup(i.content, features="html.parser")
-    player_num = player_page.find(
-        "a", {"class": "spielprofil_tooltip"})["href"]
+    try:
+        player_num = player_page.find(
+            "a", {"class": "spielprofil_tooltip"})["href"]
+    except TypeError:
+        print(f'O jogador {player} não foi encontrado')
+        continue
+
     p = player_num.split("/")
     new_url = base + p[1] + "/leistungsdaten/spieler/" + \
         p[-1]+"/plus/0?saison=ges"
     url_player.append(new_url)
+
 
 a = get_stats(url_player)
 read_json(a, "dataplayers2.0")
